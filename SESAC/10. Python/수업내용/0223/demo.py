96af8928e2a0247c567203813bf766e21a566050
Jiyeon <yoo2658@naver.com>
2022-02-23 17:53:45 +0900

    0223 수업내용

---
+++ SESAC/10. Python/수업내용/0223/demo.py
@@ -0,0 +1,15 @@
+import os
+
+#os.system("notepad") 메모장 오픈
+#print("음")
+#os.system("mspaint") 그림판 오픈
+#print("냐")
+#os.system("notepad") 메모장 오픈
+#print("뤼")
+
+print("abc")
+input()
+os.system("cls")
+input()
+os.system("cls")
+print("cbc")
---
+++ SESAC/10. Python/수업내용/0223/dummy2.py
@@ -0,0 +1,4 @@
+def dprint(s, n=1):
+  for i in range(n):
+    print(f"{i+1}:{s}")
+dprint("hello",3)
